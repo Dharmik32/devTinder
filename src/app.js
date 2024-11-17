17c554422386cc20db846189293392a78e08f1eb
Dharmik32 <dharmikchavda07@gmail.com>
2024-11-17 15:11:55 +0530

    dynamic data in signup API

--- src/app.js
+++ src/app.js
@@ -3,16 +3,11 @@ const connectDB = require("./config/database");
 const app = express();
 const User = require("./models/user");
 
+app.use(express.json());
+
 app.post("/signup", async (req, res) => {
   // Create a new instance of the User model
-  const user = new User({
-    firstName: "Sachin",
-    lastName: "Tendulkar",
-    emailId: "sachin@gmail.com",
-    password: "password123",
-  });
-
-  console.log("post app call /singup", user);
+  const user = new User(req.body);
 
   try {
     await user.save();
