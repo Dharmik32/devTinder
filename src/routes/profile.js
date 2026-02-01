9cf363fc2ecc2e52d4d42fca3b149c6133e09cba
Dharmik <dharmikchavda07@gmail.com>
2026-02-01 22:26:45 +0530

    Added logout endpoint and updated profile routes with edit functionality and validation

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const profileRouter = express.Router();
 const { userAuth } = require("../middlewares/auth");
+const { validateEditProfileData } = require("../utils/validation");
 
-profileRouter.get("/profile", userAuth, async (req, res) => {
+profileRouter.get("/profile/view", userAuth, async (req, res) => {
   try {
     const user = req.user;
 
@@ -11,6 +12,26 @@ profileRouter.get("/profile", userAuth, async (req, res) => {
     }
 
     res.send(user);
+  } catch (error) {
+    res.status(400).send("ERROR : " + error.message);
+  }
+});
+
+profileRouter.patch("/profile/edit", userAuth, async (req, res) => {
+  try {
+    if (!validateEditProfileData(req)) {
+      throw new Error("Invalid Edit Request");
+    }
+
+    const loggedInUser = req.user;
+
+    Object.keys(req.body).forEach((key) => {
+      loggedInUser[key] = req.body[key];
+    });
+
+    await loggedInUser.save();
+
+    res.json({ message: "Profile Updated Successfully!", data: loggedInUser });
   } catch (error) {
     res.status(400).send("ERROR : " + error.message);
   }
